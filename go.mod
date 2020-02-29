cb94d74ea56c184ae432662afd9fff7736338d11
Arseniy Kucher <beeffallo@gmail.com>
2020-02-29 14:35:00 +0200

    encio package from orsenkucher/nothing

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/orsenkucher/parsing-platform
+
+go 1.13
+
+require github.com/orsenkucher/nothing/encio v0.0.0-20200229123326-837dee75920c
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/orsenkucher/nothing/encio"
+)
 
 func main() {
 	fmt.Println("go works")
+	key := encio.NewEncIO("password")
+	cfg, err := key.GetConfig("bot.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	fmt.Println(cfg)
 }
