97fa45b1915be41109bc2986b2b317acccd6dcfb
Arseniy Kucher <beeffallo@gmail.com>
2020-03-28 23:18:24 +0200

    ppdrop

--- main.go
+++ main.go
@@ -4,16 +4,16 @@ import (
 	"flag"
 
 	"github.com/orsenkucher/nothing/encio"
-	"github.com/orsenkucher/parsing-platform/server"
+	"github.com/orsenkucher/parsing-platform/ppdrop"
 )
 
 func main() {
 	var s = flag.String("s", "", "provide encio password")
 	flag.Parse()
 	key := encio.NewEncIO(*s)
-	bot := server.NewBot(key, "server/bot.json")
-	admin := server.NewBot(key, "server/admin.bot.json")
+	bot := ppdrop.NewBot(key, "creds/bot.json")
+	admin := ppdrop.NewBot(key, "creds/admin.bot.json")
 	go bot.Listen()
 	go admin.Listen()
-	server.StartServer(bot, admin)
+	ppdrop.StartServer(bot, admin)
 }
--- ppdrop/data.go
+++ ppdrop/data.go
@@ -1,4 +1,4 @@
-package server
+package ppdrop
 
 import (
 	"fmt"
